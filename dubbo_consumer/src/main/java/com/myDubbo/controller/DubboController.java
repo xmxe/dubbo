6071d4f08fcb5d3a0a6d417363768a805e358ca8
xmxe <464817304@qq.com>
2019-05-30 09:53:57 +0800

    5.30

---
+++ dubbo_consumer/src/main/java/com/myDubbo/controller/DubboController.java
@@ -0,0 +1,18 @@
+package com.myDubbo.controller;
+
+import com.myDubbo.service.HelloDubboService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DubboController {
+    @Autowired
+    HelloDubboService dubboService;
+
+    @RequestMapping("dubboT")
+    public String dubboT(String name){
+        return dubboService.helloDubbo(name);
+    }
+
+}
--- dubbo_provider/src/main/java/com/myDubbo/provider/service/HelloDubboServiceImpl.java
+++ dubbo_provider/src/main/java/com/myDubbo/provider/service/HelloDubboServiceImpl.java
@@ -2,9 +2,10 @@ package com.myDubbo.provider.service;
 
 import com.myDubbo.service.HelloDubboService;
 
+
 public class HelloDubboServiceImpl implements HelloDubboService{
     @Override
     public String helloDubbo(String name) {
-        return "hello:" + name;
+        return "这是provider1提供的:" + name;
     }
 }
